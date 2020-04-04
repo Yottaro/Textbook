c5240f532e3cfc991773cc7f9af0d5e3f191d569
Yottaro <52913283+Yottaro@users.noreply.github.com>
2020-04-04 19:48:50 +0900

    Update 10.リスト(2).py

--- Python/10.リスト(2).py
+++ Python/10.リスト(2).py
@@ -1,15 +1,15 @@
-list = []
+my_list = []
 
 print('コンピュータさん：5個の要素の値を入力して下さい。')
 
 for i in range(1, 6):
 
     print('あなた(', i, '回目)：', end=' ')
-    list.append(int(input()))
+    my_list.append(int(input()))
 
 print()
 print('コンピュータさん：リストができました。')
-print(list)
+print(my_list)
 
 print()
 print('コンピュータさん：リストに対して何を行いますか？')
@@ -27,7 +27,7 @@ if (a == 1):
 
     for i in range(0, 5):
         
-        list[i] = list[i] * 2
+        my_list[i] = my_list[i] * 2
 
 #　■すべて0にする■
 elif (a == 2):
@@ -36,31 +36,31 @@ elif (a == 2):
 
     for i in range(0, 5):
         
-        list[i] = 0
+        my_list[i] = 0
 
 #　■逆順にする■
 elif (a == 3):
 
     print('コンピュータさん：逆順にしました。')
-    list.reverse()
+    my_list.reverse()
 
 #　■要素を追加する■
 elif (a == 4):
 
     print('コンピュータさん：要素を追加しました。')
-    list.append(10)
+    my_list.append(10)
 
 #　■要素を削除する■
 elif (a == 5):
 
     print('コンピュータさん：要素を削除しました。')
-    list.pop(-1)
+    my_list.pop(-1)
 
 else:
 
     print('コンピュータさん：入力されたコマンドは無効です。')
 
-print(list)
+print(my_list)
 print()
 
 #　■要点■
